7fd9ffdef4501e482a46c1299d8480a588b9341e
Fabrizio Arias <fabrisal@live.com.ar>
2024-05-15 19:08:34 -0300

    Entidad Materia
    se crearon todos los datos de materia, sus getter y setter y constructores

--- ProyectoTransversal/ProyectoTransversal/src/Entidades/Materia.java
+++ ProyectoTransversal/ProyectoTransversal/src/Entidades/Materia.java
@@ -2,5 +2,72 @@ package Entidades;
 
 
 public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int anio;
+    private boolean estado;
 
+    //GETTER Y SETTER
+    
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    // CONSTRUCTORES
+
+    public Materia() {
+    }
+    
+    // CONSTRUCTOR COMPLETO
+    
+    public Materia(int idMateria, String nombre, int anio, boolean estado) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+    
+    // CONSTRUCTOR SIN ID MATERIA
+    
+    public Materia(String nombre, int anio, boolean estado) {
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anio=" + anio + ", estado=" + estado + '}';
+    }
+    
+    
+    
 }
