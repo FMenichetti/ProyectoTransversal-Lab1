5751c2b0f6db9fbac712c5c43d3b54d6fd4b0966
FMenichetti <129986699+FMenichetti@users.noreply.github.com>
2024-05-15 18:47:58 -0300

    Subida

---
+++ ProyectoTransversal/ProyectoTransversal/src/AccesoDatos/Conexion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccesoDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author DELL
+ */
+public class Conexion {
+
+    private String user;
+    private String pass;
+    private String url;
+
+    public Conexion(String user, String pass, String url) {
+        this.user = user;
+        this.pass = pass;
+        this.url = url;
+    }
+
+    public Conexion() {
+    }
+
+    
+}
